40a9dee9c6d1ee24b04226f5259607ab7f931ce0
Sia <siakaramalegos@gmail.com>
2018-10-10 15:19:09 -0500

    Sets up clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -13,6 +14,7 @@ module.exports = {
     overlay: true, // show errors in browser
   },
   plugins: [
+    new CleanWebpackPlugin(['build']),
     new HTMLWebpackPlugin({title: 'Blog  |  Home'})
   ]
 }
