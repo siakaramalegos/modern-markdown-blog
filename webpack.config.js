8d39999a2259212707919a75ef49d30f6e97497e
Sia <siakaramalegos@gmail.com>
2018-10-10 15:16:30 -0500

    Sets up dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build')
   },
+  devServer: {
+    stats: 'errors-only', // show only errors
+    // open: true, // auto open the page in browser
+    overlay: true, // show errors in browser
+  },
   plugins: [
-    new HTMLWebpackPlugin({title: 'Sia.Studio  |  Home'})
+    new HTMLWebpackPlugin({title: 'Blog  |  Home'})
   ]
 }
