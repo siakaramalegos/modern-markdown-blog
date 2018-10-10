9feb05e215e314004f762514309792f382070d8b
Sia <siakaramalegos@gmail.com>
2018-10-10 17:02:31 -0500

    Generates html string from template and markdown

--- convertPosts.js
+++ convertPosts.js
@@ -1,26 +1,40 @@
 const fs = require('fs')
 const path = require('path')
+const Remarkable = require('remarkable')
+const hljs = require('highlight.js')
+const ejs = require('ejs')
+
+// Layout file to use for all posts
+const layout = fs.readFileSync(path.resolve(__dirname, './posts/layout.ejs'), 'utf8')
+// const layout = require('./posts/layout.ejs')
+
+// Sample markdown post file
 const mdFile = fs.readFileSync(path.resolve(__dirname, './posts/my_post.md'), 'utf8')
 
-const Remarkable = require('remarkable')
-// const hljs = require('highlight.js')
-const md = new Remarkable()
+// Initialize markdown function with syntax highlighting by highlight.js
+var md = new Remarkable({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) { }
+    }
 
-// // Actual default values
-// var md = new Remarkable({
-//   highlight: function (str, lang) {
-//     if (lang && hljs.getLanguage(lang)) {
-//       try {
-//         return hljs.highlight(lang, str).value;
-//       } catch (err) { }
-//     }
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) { }
 
-//     try {
-//       return hljs.highlightAuto(str).value;
-//     } catch (err) { }
+    return ''; // use external default escaping
+  }
+});
 
-//     return ''; // use external default escaping
-//   }
-// });
+// Convert sample markdown file to html
+const markdown = md.render(`${mdFile}`)
 
-console.log(md.render(`${mdFile}`))
+// Data associated with sample post
+const context = {title: 'My First Post', body: String(markdown)}
+
+// Generate the full html for the sample post's page using its data
+const html = ejs.render(layout, context)
+
+console.log(html);
