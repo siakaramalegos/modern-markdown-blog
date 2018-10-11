4f04748ac4e32f685dde24e0fbc7edd631a0c89f
Sia <siakaramalegos@gmail.com>
2018-10-11 18:30:36 -0500

    Loads css with autoprefixer and minification

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+// Import files so webpack can see them
+import './style.css'
 console.log('index.js is loaded');
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'file-loader', options: { name: '[name].[ext]' } },
+          'extract-loader',
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'postcss-loader', options: {
+            plugins: () => [
+              require('autoprefixer')(),
+              require('cssnano')(),
+            ],
+          }},
+        ]
+      },
+    ],
+  },
   devServer: {
     stats: 'errors-only', // show only errors
     // open: true, // auto open the page in browser
